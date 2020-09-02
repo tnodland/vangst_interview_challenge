0e8afbefcfc3d53a86f27edcbe1d7c41749d774e
Trevor Nodland <trevor.nodland@gmail.com>
2020-09-02 15:30:21 -0600

    begins adding getCandidate query to query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,10 +5,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :getCandidate
+          [Types::CandidateType],
+          null: false,
+          description: "Returns a single candidate by id"
+
     field :hello_world, # name of method
           String, # Type the data will be
           null: false, # If it can ever return a null value
           description: 'An example field added for your reference!' # Optional Descriotion
+
+    def getCandidate
+      Candidate.get()
+    end
 
     def hello_world
       'Hello World - This is your Interview Challenge!\nGood luck.'
