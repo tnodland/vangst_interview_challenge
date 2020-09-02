fce40f3e57346f854a51a8d1d292d7c0ff411688
Trevor Nodland <trevor.nodland@gmail.com>
2020-09-02 14:59:55 -0600

    adds candidate query type to the graphql query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,10 +5,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :candidates,
+          [Types::CandidateType],
+          null: false,
+          description: "Returns a list of all candidates"
+
     field :hello_world, # name of method
           String, # Type the data will be
           null: false, # If it can ever return a null value
           description: 'An example field added for your reference!' # Optional Descriotion
+
+    def candidates
+      Candidate.all
+    end
 
     def hello_world
       'Hello World - This is your Interview Challenge!\nGood luck.'
